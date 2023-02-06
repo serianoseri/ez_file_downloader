d69b57e1280436a5eee481e4cc0ddac5ef4da7cf
SteveSevetS <steve.joker00@gmail.com>
2023-02-06 15:47:27 +0100

    fixed backslash and slash issue

--- batch_downloader.py
+++ batch_downloader.py
@@ -70,7 +70,7 @@ def download(url: str, dest_dir: str):
         dest_path = os.path.join(dest_dir, filename)
         task_id = progress.add_task("Downloading", filename=filename, start=False, visible=True)
         copy_url(task_id, url, dest_path)
-        progress.refresh()
+        #progress.refresh() not needed if auto_refresh=True in the Progress obj constuctor
         task_id = ''
         return
 
@@ -87,6 +87,21 @@ def check_in_path(path):
         cprint('Invalid argument, not of type path or invalid path', 'red', file=sys.stderr)
         argParser.print_help(sys.stderr)
         sys.exit(2)
+
+'''
+checking the os for selecting the path separator 
+    \ on windows
+    / on linux/macOS
+    if the user is on a different OS it will stop the execution of the script telling the user that is on an unsupported os
+'''
+slash = ''
+if sys.platform == 'win32':
+    slash = '\\'
+elif sys.platform == 'linux' or sys.platform == 'darwin':
+    slash = '/'
+else:
+    print("[EROOR] you are on an unsupported operating system")
+    exit(1)
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-l", "--link", help = "txt file with downloadable links in it", type = check_in_path, required = True)
@@ -107,17 +122,17 @@ for link in links:
     filename = os.path.join(current, name)
     
     if link.startswith('/folder/'):
-        newpath = dir_downloads + '\\' + link.replace('/folder/', '')
+        newpath = dir_downloads + slash + link.replace('/folder/', '')
         current = newpath
         ensure_dir(newpath)
     elif link.startswith('/hls_name/'):
         hls_name = link.replace('/hls_name/', '')
-    elif link.startswith('/hls/') and not os.path.isfile(current + '\\' + hls_name + '.mp4'):
+    elif link.startswith('/hls/') and not os.path.isfile(current + slash + hls_name + '.mp4'):
         hls = link.replace('/hls/', '', 1)
         print("hls-name: " + hls_name)
         print("hls-link: " + hls)
         command_to_use = ffmpeg_hls_stream.replace('|link|', hls)
-        command_to_use = command_to_use.replace('|pathname|', current + '\\' + hls_name)
+        command_to_use = command_to_use.replace('|pathname|', current + slash + hls_name)
         
         os.system(f'cmd /c "{command_to_use}"')
         hls = ''
